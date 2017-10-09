09c05238f0961429c802f891c057d8ceba09d1a2
yingy4 <yingy4@hotmail.com>
2017-10-09 09:50:04 +0800

    Delete Build.scala

--- project/Build.scala
+++
@@ -1,7 +0,0 @@
-import sbt._
-import Keys._
-
-object MultipleModuleProjectBuild extends Build {
-    lazy val root = Project(id = "Darwin", base = file(".")) dependsOn(LaScala)
-    lazy val LaScala = Project(id = "LaScala", base = file("LaScala"))
-}
