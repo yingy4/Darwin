ef86dc49e8df70f748344b5dd9827b9884f73040
yingy4 <yingy4@hotmail.com>
2017-10-09 08:51:00 +0800

    Update Build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -2,6 +2,6 @@ import sbt._
 import Keys._
 
 object MultipleModuleProjectBuild extends Build {
-    lazy val root = Project(id = "Darwin", base = file(".")) aggregate(LaScala)
+    lazy val root = Project(id = "Darwin", base = file(".")) dependsOn(LaScala)
     lazy val LaScala = Project(id = "LaScala", base = file("LaScala"))
 }
